da3047740c69699d69d45651bd211f0a95b9cff2
JaesungLeee <viki0114@naver.com>
2023-05-13 00:43:00 +0900

    [feat] #1 XML + Compose 상호운용 확인

--- presentation/src/main/java/com/mashup/presentation/MainActivity.kt
+++ presentation/src/main/java/com/mashup/presentation/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.mashup.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+import com.mashup.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        // XML + Compose 사용 샘플
+        binding.composeView.setContent {
+            Text(text = "Hello World")
+        }
     }
 }
