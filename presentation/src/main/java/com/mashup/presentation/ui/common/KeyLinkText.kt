53d20b839fb3ee97476217fc4d69416816ca7ad5
sooziini <soojinpark@SOOJINui-MacBookAir.local>
2023-06-03 20:32:15 +0900

    [feat] #12 keylink 공통 text 구현

---
+++ presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkText.kt
@@ -0,0 +1,33 @@
+package com.mashup.presentation.ui.common
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.mashup.presentation.ui.theme.Mint
+
+@Composable
+fun KeyLinkText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            lineHeight = 28.sp,
+            color = Mint.copy(alpha = 0.7f),
+            shadow = Shadow(
+                color = Mint,
+                blurRadius = 35f
+            )
+        )
+    )
+}
