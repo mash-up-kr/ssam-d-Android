2bbb72d1ab9d05d802495599068ea3ae45d158db
JaesungLeee <viki0114@naver.com>
2023-06-04 03:18:55 +0900

    [feat] #11 스크린 추가 및 그래프 세팅

---
+++ presentation/src/main/java/com/mashup/presentation/home/ReceivedSignalFragment.kt
@@ -0,0 +1,14 @@
+package com.mashup.presentation.home
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.databinding.FragmentReceivedSignalBinding
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/03
+ */
+class ReceivedSignalFragment :
+    BaseFragment<FragmentReceivedSignalBinding>(R.layout.fragment_received_signal) {
+}
---
+++ presentation/src/main/java/com/mashup/presentation/profile/SendSignalFragment.kt
@@ -0,0 +1,13 @@
+package com.mashup.presentation.profile
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.databinding.FragmentSendSignalBinding
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/03
+ */
+class SendSignalFragment : BaseFragment<FragmentSendSignalBinding>(R.layout.fragment_send_signal) {
+}
