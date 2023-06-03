fcf6d8b5584fbce92720aed8caf1b68c00c10995
JaesungLeee <viki0114@naver.com>
2023-06-04 03:18:55 +0900

    [feat] #11 스크린 추가 및 그래프 세팅

---
+++ presentation/src/main/java/com/mashup/presentation/home/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.mashup.presentation.home
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseActivity
+import com.mashup.presentation.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/03
+ */
+@AndroidEntryPoint
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home)
---
+++ presentation/src/main/java/com/mashup/presentation/profile/OpenSourceFragment.kt
@@ -0,0 +1,13 @@
+package com.mashup.presentation.profile
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.databinding.FragmentOpenSourceBinding
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/03
+ */
+class OpenSourceFragment : BaseFragment<FragmentOpenSourceBinding>(R.layout.fragment_open_source) {
+}
