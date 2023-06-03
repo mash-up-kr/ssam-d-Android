8c3ed8e1954de5d79f898ba48d63d42fc4d6133c
JaesungLeee <viki0114@naver.com>
2023-06-03 16:52:04 +0900

    [feat] #11 시그널 보내기 화면 및 그래프 생성

---
+++ presentation/src/main/java/com/mashup/presentation/signal/SignalCompleteFragment.kt
@@ -0,0 +1,18 @@
+package com.mashup.presentation.signal
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.databinding.FragmentSignalCompleteBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/03
+ */
+@AndroidEntryPoint
+class SignalCompleteFragment :
+    BaseFragment<FragmentSignalCompleteBinding>(R.layout.fragment_signal_complete) {
+
+    override fun initViews() {}
+}
