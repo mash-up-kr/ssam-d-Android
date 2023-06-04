0575102bf60996618018ffc223c2a1f1613f66b4
JaesungLeee <viki0114@naver.com>
2023-06-04 16:59:27 +0900

    [feat] #11 BottomNavigation 연결

--- presentation/src/main/java/com/mashup/presentation/home/HomeActivity.kt
+++ presentation/src/main/java/com/mashup/presentation/home/HomeActivity.kt
@@ -1,5 +1,8 @@
 package com.mashup.presentation.home
 
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseActivity
 import com.mashup.presentation.databinding.ActivityHomeBinding
@@ -11,4 +14,25 @@ import dagger.hilt.android.AndroidEntryPoint
  * @created 2023/06/03
  */
 @AndroidEntryPoint
-class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home)
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(binding.fcvHome.id) as NavHostFragment
+    }
+    private val navController by lazy { navHostFragment.navController }
+
+    override fun initViews() {
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        binding.bnvHome.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvHome.visibility = when (destination.id) {
+                /* 디자인 명세에 따라 수정 */
+                R.id.homeFragment, R.id.reelsFragment -> View.VISIBLE
+                else -> View.GONE
+            }
+        }
+    }
+}
