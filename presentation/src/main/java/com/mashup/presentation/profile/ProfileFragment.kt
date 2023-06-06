94dea6aa6e412bc877d86c5a8d9391067c845c7e
JaesungLeee <viki0114@naver.com>
2023-06-06 15:36:08 +0900

    [feat] #19 클릭 리스너 등록

--- presentation/src/main/java/com/mashup/presentation/profile/ProfileFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/profile/ProfileFragment.kt
@@ -2,6 +2,7 @@ package com.mashup.presentation.profile
 
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.common.extension.navigate
 import com.mashup.presentation.databinding.FragmentProfileBinding
 
 /**
@@ -10,4 +11,30 @@ import com.mashup.presentation.databinding.FragmentProfileBinding
  * @created 2023/06/03
  */
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    override fun initViews() {
+        initClickListeners()
+    }
+
+    private fun initClickListeners() {
+        with(binding) {
+            btnUpdateUserName.setOnClickListener {
+                /* 다이얼로그 */
+            }
+            btnNotificationDetail.setOnClickListener {
+                navigate(R.id.action_profile_to_notificationSetting)
+            }
+            btnTosDetail.setOnClickListener {
+                navigate(R.id.action_profile_to_termsOfService)
+            }
+            btnPrivacyPolicyDetail.setOnClickListener {
+                navigate(R.id.action_profile_to_privacyPolicy)
+            }
+            btnOpenSourceDetail.setOnClickListener {
+                navigate(R.id.action_profile_to_openSource)
+            }
+            btnLogout.setOnClickListener {
+                /* 로그아웃 로직 */
+            }
+        }
+    }
 }
