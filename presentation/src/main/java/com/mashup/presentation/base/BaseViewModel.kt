5473f61bbc6e238a421c0796f6943b0f85fec541
sooziini <soojinpar1026@gmail.com>
2023-06-06 17:36:46 +0900

    [feat] #4 viewModel에서 context 사용을 위한 BaseViewModel 생성

---
+++ presentation/src/main/java/com/mashup/presentation/base/BaseViewModel.kt
@@ -0,0 +1,16 @@
+package com.mashup.presentation.base
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+open class BaseViewModel @Inject constructor(
+    application: Application
+) : AndroidViewModel(application) {
+
+    protected val context: Context
+        get() = getApplication<Application>().applicationContext
+}
