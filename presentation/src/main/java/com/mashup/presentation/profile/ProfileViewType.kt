570d151542c4f2ac8305060edc5ed4e6c20880fc
JaesungLeee <viki0114@naver.com>
2023-06-07 16:20:10 +0900

    [feat] #19 ProfileViewType 생성 및 추상화

---
+++ presentation/src/main/java/com/mashup/presentation/profile/ProfileViewType.kt
@@ -0,0 +1,49 @@
+package com.mashup.presentation.profile
+
+import androidx.annotation.DrawableRes
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/07
+ */
+sealed class ProfileViewType {
+
+    interface BaseViewType {
+        val id: Int
+    }
+
+    interface ClickableViewType {
+        val onClick: () -> Unit
+    }
+
+    data class UserInfo(
+        override val id: Int,
+        override val onClick: () -> Unit,
+        val userName: String,
+        val userEmail: String,
+        val userAvatarImage: String,
+    ) : BaseViewType, ClickableViewType, ProfileViewType()
+
+    data class Header(
+        override val id: Int,
+        val description: String,
+    ) : BaseViewType, ProfileViewType()
+
+    data class NavigationContent(
+        override val id: Int,
+        override val onClick: () -> Unit,
+        val description: String,
+        @DrawableRes val drawableRes: Int,
+    ) : BaseViewType, ClickableViewType, ProfileViewType()
+
+    data class AppVersionContent(
+        override val id: Int,
+        val version: String,
+    ) : BaseViewType, ProfileViewType()
+
+    data class LogoutContent(
+        override val id: Int,
+        override val onClick: () -> Unit,
+    ) : BaseViewType, ClickableViewType, ProfileViewType()
+}
