303c15fe9358a121bfc23e7542b12497c42d669e
JaesungLeee <viki0114@naver.com>
2023-06-07 16:59:09 +0900

    [feat] #19 마이페이지 멀티뷰타입 Adapter 추가

---
+++ presentation/src/main/java/com/mashup/presentation/profile/ProfileAdapter.kt
@@ -0,0 +1,155 @@
+package com.mashup.presentation.profile
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mashup.presentation.R
+import com.mashup.presentation.databinding.*
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/07
+ */
+class ProfileAdapter(private val onButtonClick: (ClickEvent) -> Unit) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var optionsList: List<ProfileViewType> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            R.layout.item_profile_user_info -> {
+                val binding = ItemProfileUserInfoBinding.inflate(layoutInflater, parent, false)
+                UserInfoViewHolder(binding) {
+                    onButtonClick.invoke(ClickEvent.UPDATE_NAME)
+                }
+            }
+            R.layout.item_profile_header -> {
+                val binding = ItemProfileHeaderBinding.inflate(layoutInflater, parent, false)
+                HeaderViewHolder(binding)
+            }
+            R.layout.item_profile_navigation_content -> {
+                val binding =
+                    ItemProfileNavigationContentBinding.inflate(layoutInflater, parent, false)
+                NavigationContentViewHolder(binding) {
+                    onButtonClick.invoke(ClickEvent.NAVIGATE)
+                }
+            }
+            R.layout.item_profile_app_version_content -> {
+                val binding =
+                    ItemProfileAppVersionContentBinding.inflate(layoutInflater, parent, false)
+                AppVersionViewHolder(binding)
+            }
+            R.layout.item_profile_logout_content -> {
+                val binding = ItemProfileLogoutContentBinding.inflate(layoutInflater, parent, false)
+                LogoutViewHolder(binding) {
+                    onButtonClick.invoke(ClickEvent.LOGOUT)
+                }
+            }
+            else -> throw IllegalStateException("Unknown ViewType $viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val option = optionsList[position]) {
+            is ProfileViewType.UserInfo -> (holder as UserInfoViewHolder).bindItems(option)
+            is ProfileViewType.Header -> (holder as HeaderViewHolder).bindItems(option)
+            is ProfileViewType.NavigationContent ->
+                (holder as NavigationContentViewHolder).bindItems(option)
+            is ProfileViewType.AppVersionContent ->
+                (holder as AppVersionViewHolder).bindItems(option)
+            is ProfileViewType.LogoutContent -> (holder as LogoutViewHolder)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (optionsList[position]) {
+            is ProfileViewType.UserInfo -> R.layout.item_profile_user_info
+            is ProfileViewType.Header -> R.layout.item_profile_header
+            is ProfileViewType.NavigationContent -> R.layout.item_profile_navigation_content
+            is ProfileViewType.AppVersionContent -> R.layout.item_profile_app_version_content
+            is ProfileViewType.LogoutContent -> R.layout.item_profile_logout_content
+        }
+    }
+
+    override fun getItemCount(): Int = optionsList.size
+
+    class UserInfoViewHolder(
+        private val binding: ItemProfileUserInfoBinding,
+        onUpdateButtonClick: () -> Unit,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.btnUpdateUserName.setOnClickListener {
+                onUpdateButtonClick.invoke()
+            }
+        }
+
+        fun bindItems(item: ProfileViewType.UserInfo) {
+            with(binding) {
+                userInfo = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    class HeaderViewHolder(private val binding: ItemProfileHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindItems(item: ProfileViewType.Header) {
+            with(binding) {
+                header = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    class NavigationContentViewHolder(
+        private val binding: ItemProfileNavigationContentBinding,
+        onNavigateButtonClick: () -> Unit,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.btnNavigationIcon.setOnClickListener {
+                onNavigateButtonClick.invoke()
+            }
+        }
+
+        fun bindItems(item: ProfileViewType.NavigationContent) {
+            with(binding) {
+                content = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    class AppVersionViewHolder(private val binding: ItemProfileAppVersionContentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindItems(item: ProfileViewType.AppVersionContent) {
+            with(binding) {
+                content = item
+                executePendingBindings()
+            }
+        }
+    }
+
+    class LogoutViewHolder(
+        private val binding: ItemProfileLogoutContentBinding,
+        onLogoutButtonClick: () -> Unit,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.btnLogout.setOnClickListener {
+                onLogoutButtonClick.invoke()
+            }
+        }
+    }
+
+    enum class ClickEvent {
+        UPDATE_NAME, NAVIGATE, LOGOUT
+    }
+}
