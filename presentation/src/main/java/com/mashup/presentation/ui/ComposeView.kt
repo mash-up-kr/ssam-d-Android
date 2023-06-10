1715e05be83cee73a1cb749a5badf77a7b17bdb8
Huijiny <jiin20803@gmail.com>
2023-06-10 22:22:05 +0900

    [feat] #8 ComposeView 테마 적용 setContent extension 생성

--- presentation/src/main/java/com/mashup/presentation/ui/ComposeView.kt
+++ presentation/src/main/java/com/mashup/presentation/ui/ComposeView.kt
@@ -7,28 +7,22 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import com.mashup.presentation.ui.theme.SsamDTheme
 
+@Suppress("NOTHING_TO_INLINE")
 inline fun ComponentActivity.setThemeContent(
-    crossinline content: @Composable () -> Unit
+    noinline content: @Composable () -> Unit
 ) = setContent {
     SsamDTheme {
         content()
     }
 }
 
+@Suppress("NOTHING_TO_INLINE")
 inline fun Fragment.setThemeContent(
-    crossinline content: @Composable () -> Unit
+    noinline content: @Composable () -> Unit
 ) = ComposeView(requireContext()).apply {
     setContent {
         SsamDTheme {
             content()
         }
-    }
-}
-
-inline fun ComposeView.setThemeContent(
-    crossinline content: @Composable () -> Unit
-) = setContent {
-    SsamDTheme {
-        content()
     }
 }
