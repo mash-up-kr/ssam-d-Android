2af56b98d4adf883a253803474652bc1fbc5a2cf
Huijiny <jiin20803@gmail.com>
2023-06-12 00:48:09 +0900

    [feat] #8 setContentView 삭제

--- presentation/src/main/java/com/mashup/presentation/onboarding/OnBoardingActivity.kt
+++ presentation/src/main/java/com/mashup/presentation/onboarding/OnBoardingActivity.kt
@@ -1,37 +1,14 @@
 package com.mashup.presentation.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import com.mashup.presentation.R
-import com.mashup.presentation.databinding.ActivityMainBinding
-import com.mashup.presentation.databinding.ActivityOnBoardingBinding
-import com.mashup.presentation.ui.common.KeyLinkButton
-import com.mashup.presentation.ui.common.KeyLinkTextField
+import androidx.activity.ComponentActivity
 import com.mashup.presentation.ui.setThemeContent
-import com.mashup.presentation.ui.theme.SsamDTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OnBoardingActivity : AppCompatActivity() {
-
-    private val binding: ActivityOnBoardingBinding by lazy {
-        ActivityOnBoardingBinding.inflate(
-            layoutInflater
-        )
-    }
-
+class OnBoardingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
         setThemeContent {
             OnBoardingScreen()
         }
