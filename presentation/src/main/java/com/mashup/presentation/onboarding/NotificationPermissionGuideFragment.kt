23c06435fdcc552c93f6b37a96f6d23a3a0f8052
JaesungLeee <viki0114@naver.com>
2023-06-14 15:18:54 +0900

    [feat] #31 화면 자체에서 권한 요청하도록 로직 추가

--- presentation/src/main/java/com/mashup/presentation/onboarding/NotificationPermissionGuideFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/onboarding/NotificationPermissionGuideFragment.kt
@@ -1,14 +1,20 @@
 package com.mashup.presentation.onboarding
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
+import android.net.Uri
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.common.util.permission.PermissionType
+import com.mashup.presentation.common.util.permission.manager.PermissionManager
+import com.mashup.presentation.common.util.permission.manager.PermissionManagerImpl
 import com.mashup.presentation.databinding.FragmentNotificationPermissionGuideBinding
 import com.mashup.presentation.ui.common.KeyLinkMintText
 import com.mashup.presentation.ui.setThemeContent
@@ -25,6 +31,16 @@ class NotificationPermissionGuideFragment :
     BaseFragment<FragmentNotificationPermissionGuideBinding>(
         R.layout.fragment_notification_permission_guide
     ) {
+
+    private val requestNotificationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(requireActivity(), "권한 허용", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireActivity(), "권한 거부", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun initViews() {
         binding.cpvTitle.setThemeContent {
             KeyLinkMintText(
@@ -37,36 +53,31 @@ class NotificationPermissionGuideFragment :
             requestNotificationPermission()
         }
     }
-    private val requestNotificationPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                Toast.makeText(requireActivity(), "권한 허용", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(requireActivity(), "권한 거부", Toast.LENGTH_SHORT).show()
-            }
-        }
 
     private fun requestNotificationPermission() {
-        if (ContextCompat.checkSelfPermission(
+        when {
+            ContextCompat.checkSelfPermission(
                 requireActivity(),
                 Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                // 다른 런타임 퍼미션이랑 비슷한 과정
-                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                    // 왜 알림을 허용해야하는지 유저에게 알려주기를 권장
-                    Toast.makeText(requireContext(), "클릭 1", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(requireContext(), "클릭 2", Toast.LENGTH_SHORT).show()
-                    requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                }
-            } else {
-                Toast.makeText(requireContext(), "Android 12 이하", Toast.LENGTH_SHORT).show()
-                // 안드로이드 12 이하는 알림에 런타임 퍼미션 없으니, 설정가서 켜보라고 권해볼 수 있겠다.
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Toast.makeText(requireActivity(), "이미 권한 존재", Toast.LENGTH_SHORT).show()
             }
-        } else {
-            Timber.e("ASASDASDADAS")
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                Snackbar.make(
+                    binding.root,
+                    "설정으로 이동합니다",
+                    Snackbar.LENGTH_LONG
+                ).setAction("Settings") {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    val uri: Uri = Uri.fromParts("package", "com.mashup.ssamd", null)
+                    intent.data = uri
+                    startActivity(intent)
+                }.show()
+            }
+            else -> {
+                requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
