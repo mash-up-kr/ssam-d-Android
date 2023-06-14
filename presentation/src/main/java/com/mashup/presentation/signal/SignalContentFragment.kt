7d7a524b6b92a255bf9e2cbeabb5db9342793a80
JaesungLeee <viki0114@naver.com>
2023-06-15 02:59:08 +0900

    [feat] #43 보내기 완료 레이아웃 구성 및 navigate 연결

--- presentation/src/main/java/com/mashup/presentation/signal/SignalContentFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/signal/SignalContentFragment.kt
@@ -2,6 +2,7 @@ package com.mashup.presentation.signal
 
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.common.extension.navigate
 import com.mashup.presentation.databinding.FragmentSignalContentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +15,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class SignalContentFragment :
     BaseFragment<FragmentSignalContentBinding>(R.layout.fragment_signal_content) {
 
-    override fun initViews() {}
+    override fun initViews() {
+        binding.asd.setOnClickListener {
+            navigate(R.id.action_signalContent_to_signalComplete)
+        }
+    }
 }
