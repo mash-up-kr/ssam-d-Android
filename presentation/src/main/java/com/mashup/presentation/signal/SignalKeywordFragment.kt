ea7104d0895aab175a4c456f13d65a083cc039ac
JaesungLeee <viki0114@naver.com>
2023-06-15 02:39:54 +0900

    [feat] #43 시그널 보내기 graph 수정

---
+++ presentation/src/main/java/com/mashup/presentation/signal/SignalKeywordFragment.kt
@@ -0,0 +1,18 @@
+package com.mashup.presentation.signal
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.databinding.FragmentSignalKeywordBinding
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/15
+ */
+class SignalKeywordFragment :
+    BaseFragment<FragmentSignalKeywordBinding>(R.layout.fragment_signal_keyword) {
+
+    override fun initViews() {
+
+    }
+}
