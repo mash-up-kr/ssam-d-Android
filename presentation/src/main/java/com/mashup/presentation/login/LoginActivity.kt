3648dfb0a6fc1621d6f86d3dacefa823f81cad10
sooziini <soojinpar1026@gmail.com>
2023-06-16 23:15:10 +0900

    [feat] #32 LoginActivity 내 loginViewModel 제거

--- presentation/src/main/java/com/mashup/presentation/login/LoginActivity.kt
+++ presentation/src/main/java/com/mashup/presentation/login/LoginActivity.kt
@@ -2,14 +2,11 @@ package com.mashup.presentation.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.mashup.presentation.ui.setThemeContent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
-
-    private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
