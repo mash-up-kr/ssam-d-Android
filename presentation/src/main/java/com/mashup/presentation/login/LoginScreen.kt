75821a2584c28fd86a2dbce31f4ec2751fb41591
sooziini <soojinpar1026@gmail.com>
2023-06-17 15:15:30 +0900

    [style] #46 사용하지 않는 import 제거

--- presentation/src/main/java/com/mashup/presentation/login/LoginScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/login/LoginScreen.kt
@@ -2,7 +2,6 @@ package com.mashup.presentation.login
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
