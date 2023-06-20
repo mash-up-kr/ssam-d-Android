1e2b3b6c287be993a53c8e30527dfd9f6527d2da
sooziini <soojinpar1026@gmail.com>
2023-06-21 00:20:12 +0900

    [feat] #58 NicknameScreen text field 추가

--- presentation/src/main/java/com/mashup/presentation/login/NicknameScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/login/NicknameScreen.kt
@@ -4,32 +4,50 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mashup.presentation.R
-import com.mashup.presentation.ui.common.KeyLinkButton
-import com.mashup.presentation.ui.common.KeyLinkMintText
+import com.mashup.presentation.ui.common.*
 
 @Composable
-fun NicknameScreen(onNextButtonClicked: () -> Unit){
+fun NicknameScreen(onNextButtonClicked: (String) -> Unit){
+    var nickname by remember { mutableStateOf("") }
+    var validation by remember { mutableStateOf(ValidationState.EMPTY) }
+    val expectedText = "올바른 닉네임"
+
     Box {
         LoginBackground()
 
-        LoginContainer(modifier = Modifier.padding(top = 233.dp, bottom = 12.dp, start = 20.dp, end = 20.dp)) {
+        LoginContainer(modifier = Modifier.padding(top = 185.dp, bottom = 12.dp, start = 20.dp, end = 20.dp)) {
             Column(modifier = Modifier.weight(1f)) {
                 KeyLinkMintText(
                     modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp),
                     text = stringResource(R.string.login_nickname)
                 )
+
+                KeyLinkBoxTextField(
+                    value = nickname,
+                    onValueChange = { value ->
+                        nickname = value
+                        checkValidation(nickname, expectedText) { validationState ->
+                            validation = validationState
+                        }
+                    },
+                    hint = stringResource(R.string.login_nickname_hint),
+                    maxLength = 10,
+                    fontSize = 32.sp,
+                    validationState = validation
+                )
             }
 
             KeyLinkButton(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp),
                 text = stringResource(R.string.login_next_btn),
-                onClick = { onNextButtonClicked() },
-                enable = true
+                onClick = { onNextButtonClicked(nickname) },
+                enable = validation == ValidationState.SUCCESS
             )
         }
     }
