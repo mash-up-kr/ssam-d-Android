038a3b69da114ee8d0a1664a542812206c3607bd
JaesungLeee <viki0114@naver.com>
2023-06-21 01:02:25 +0900

    [feat] #60 Toolbar 아이콘 수정 및 title 컴포저블 가능하도록 수정

--- presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkToolbar.kt
+++ presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkToolbar.kt
@@ -4,25 +4,29 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.mashup.presentation.R
 import com.mashup.presentation.ui.theme.Black
 import com.mashup.presentation.ui.theme.White
 
 @Composable
 fun KeyLinkToolbar(
+    title: @Composable () -> Unit = {},
     onClickBack: () -> Unit
 ) {
     TopAppBar(
-        title = { },
+        title = title,
         backgroundColor = Black,
         contentColor = White,
         navigationIcon = {
             IconButton(onClick = { onClickBack() }) {
-                Icon(Icons.Default.ArrowBack, contentDescription = "백 버튼")
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_chevron_left_24),
+                    contentDescription = "백 버튼"
+                )
             }
         },
         modifier = Modifier.height(52.dp)
