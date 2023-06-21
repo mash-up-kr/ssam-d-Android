acda606184d681299b1acb503de9a933f675cb15
Huijiny <jiin20803@gmail.com>
2023-06-21 16:37:09 +0900

    [feat] #36 RemoteOnBoardingDataSource 생성

---
+++ data/src/main/java/com/mashup/data/source/remote/datasource/RemoteOnboardingDataSource.kt
@@ -0,0 +1,12 @@
+package com.mashup.data.source.remote.datasource
+
+import com.mashup.data.source.remote.service.OnboardingService
+import javax.inject.Inject
+
+class RemoteOnboardingDataSource @Inject constructor(
+    val onboardingService: OnboardingService
+) {
+    suspend fun saveOnboardingKeywords(keywords: List<String>) {
+        onboardingService.saveOnboardingKeywords(keywords)
+    }
+}
