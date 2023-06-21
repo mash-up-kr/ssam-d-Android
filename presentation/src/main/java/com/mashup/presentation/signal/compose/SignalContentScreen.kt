34d0cadcd82aeeeabb9860ee0169b3316c702372
JaesungLeee <viki0114@naver.com>
2023-06-21 13:54:05 +0900

    [feat] #60 scrollable 추가

--- presentation/src/main/java/com/mashup/presentation/signal/compose/SignalContentScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/signal/compose/SignalContentScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -73,9 +75,12 @@ fun SignalContent(
     onLengthOver: () -> Unit,
 ) {
     var text by rememberSaveable { mutableStateOf("") }
+    val scrollState = rememberScrollState()
 
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
     ) {
         Column(
             modifier = modifier.weight(1f)
