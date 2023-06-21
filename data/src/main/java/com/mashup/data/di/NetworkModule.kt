30c4718761ee42323c2a7dabed795cbacb25981e
Huijiny <jiin20803@gmail.com>
2023-06-21 16:21:08 +0900

    [feat] #36 NetworkModule 생성

---
+++ data/src/main/java/com/mashup/data/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.mashup.data.di
+
+import com.mashup.data.network.TokenInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("http://49.50.166.183:30000/")
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(tokenInterceptor: TokenInterceptor) =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(HttpLoggingInterceptor())
+            .addInterceptor(tokenInterceptor)
+            .build()
+}
