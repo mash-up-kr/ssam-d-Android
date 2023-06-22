a9f66826ef36c3b37caeb323546f93454f18561f
sooziini <soojinpar1026@gmail.com>
2023-06-22 22:21:20 +0900

    [feat] #58 KeyLinkMintText에 typography 적용

--- presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkMintText.kt
+++ presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkMintText.kt
@@ -5,29 +5,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
 import com.mashup.presentation.ui.theme.Mint
+import com.mashup.presentation.ui.theme.Typography
 
 @Composable
 fun KeyLinkMintText(
     modifier: Modifier = Modifier,
-    text: String
+    text: String,
+    textStyle: TextStyle = Typography.h4,
+    textAlign: TextAlign = TextAlign.Center
 ) {
     Text(
         modifier = modifier,
         text = text,
-        textAlign = TextAlign.Center,
-        style = TextStyle(
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 20.sp,
-            lineHeight = 28.sp,
-            color = Mint.copy(alpha = 0.7f),
-            shadow = Shadow(
-                color = Mint,
-                blurRadius = 35f
-            )
-        )
+        textAlign = textAlign,
+        color = Mint,
+        style = textStyle.copy(shadow = Shadow(color = Mint.copy(alpha = 0.7f), blurRadius = 10f))
     )
 }
