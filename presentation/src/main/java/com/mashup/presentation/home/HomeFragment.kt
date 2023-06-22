f9c06d42691e7801a7a31867f9e93214574eabd8
Huijiny <jiin20803@gmail.com>
2023-06-22 18:48:13 +0900

    [feat] #71 HomeFragment ComposeView

--- presentation/src/main/java/com/mashup/presentation/home/HomeFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.mashup.presentation.home
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
 import com.mashup.presentation.databinding.FragmentHomeBinding
+import com.mashup.presentation.ui.setThemeContent
 
 /**
  * Ssam_D_Android
@@ -10,4 +11,10 @@ import com.mashup.presentation.databinding.FragmentHomeBinding
  * @created 2023/06/04
  */
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun initViews() {
+        binding.composeView.setThemeContent {
+            HomeScreen()
+        }
+    }
 }
