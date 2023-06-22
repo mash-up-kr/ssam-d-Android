7e5a6dbeb904d27ebfad0c550d526ebbe309cbff
sooziini <soojinpar1026@gmail.com>
2023-06-22 23:53:31 +0900

    [feat] #68 Typography 파일 추가

--- presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkMintText.kt
+++ presentation/src/main/java/com/mashup/presentation/ui/common/KeyLinkMintText.kt
@@ -6,14 +6,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import com.mashup.presentation.ui.theme.Heading4
 import com.mashup.presentation.ui.theme.Mint
-import com.mashup.presentation.ui.theme.Typography
 
 @Composable
 fun KeyLinkMintText(
     modifier: Modifier = Modifier,
     text: String,
-    textStyle: TextStyle = Typography.h4,
+    textStyle: TextStyle = Heading4,
     textAlign: TextAlign = TextAlign.Center
 ) {
     Text(
