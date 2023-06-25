94f6ec5d9c08b236cfc5358ba9bb69885ef15e93
JaesungLeee <viki0114@naver.com>
2023-06-25 19:05:07 +0900

    [feat] #83 구독한 키워드 fragment 네비게이션 그래프 연결

---
+++ presentation/src/main/java/com/mashup/presentation/home/SubscribeKeywordFragment.kt
@@ -0,0 +1,20 @@
+package com.mashup.presentation.home
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.common.extension.setThemeContent
+import com.mashup.presentation.databinding.FragmentSubscribeKeywordBinding
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/25
+ */
+class SubscribeKeywordFragment : BaseFragment<FragmentSubscribeKeywordBinding>(R.layout.fragment_subscribe_keyword) {
+
+    override fun initViews() {
+        binding.composeView.setThemeContent {
+
+        }
+    }
+}
