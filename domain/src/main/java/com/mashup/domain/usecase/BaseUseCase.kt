f4f6d28fcace3ab12d8f29e1ae3fbbd23aaa5bff
Huijiny <jiin20803@gmail.com>
2023-06-25 17:47:57 +0900

    [feat] #36 Usecase에 로그 추가

--- domain/src/main/java/com/mashup/domain/usecase/BaseUseCase.kt
+++ domain/src/main/java/com/mashup/domain/usecase/BaseUseCase.kt
@@ -1,10 +1,14 @@
 package com.mashup.domain.usecase
 
+import timber.log.Timber
+
 abstract class BaseUseCase<I, O> {
     protected abstract suspend fun invoke(param: I): O
 
-    suspend fun execute(param: I) {
-        // TODO: 로그를 붙이거나, coroutineScope를 정하거나 등
-        invoke(param)
+    suspend fun execute(param: I): O {
+        Timber.i("param : $param")
+        return invoke(param).also {
+            Timber.i("result : $it")
+        }
     }
 }
