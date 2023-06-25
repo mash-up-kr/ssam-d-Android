c7e77d53399520277b0b850724dce7aa3c8afccf
Huijiny <jiin20803@gmail.com>
2023-06-25 18:11:03 +0900

    [feat] #36 로그 제거

--- domain/src/main/java/com/mashup/domain/usecase/BaseUseCase.kt
+++ domain/src/main/java/com/mashup/domain/usecase/BaseUseCase.kt
@@ -1,14 +1,10 @@
 package com.mashup.domain.usecase
 
-import timber.log.Timber
 
 abstract class BaseUseCase<I, O> {
     protected abstract suspend fun invoke(param: I): O
 
     suspend fun execute(param: I): O {
-        Timber.i("param : $param")
-        return invoke(param).also {
-            Timber.i("result : $it")
-        }
+        return invoke(param)
     }
 }
