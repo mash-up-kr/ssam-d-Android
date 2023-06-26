0b011e9009f916002cb26ba0777cc51ec53532e3
JaesungLeee <viki0114@naver.com>
2023-06-27 03:03:15 +0900

    [style] #90 파일 분리

---
+++ presentation/src/main/java/com/mashup/presentation/chat/compose/ChatListScreen.kt
@@ -0,0 +1,13 @@
+package com.mashup.presentation.chat.compose
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/06/27
+ */
+@Composable
+fun ChatListScreen() {
+
+}
