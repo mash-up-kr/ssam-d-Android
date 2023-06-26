dd3a503821cc5d12b955f711046142db1085b6a3
JaesungLeee <viki0114@naver.com>
2023-06-27 02:53:45 +0900

    [feat] #90 ChatFragment 네비게이션 그래프 연결

--- presentation/src/main/java/com/mashup/presentation/chat/ChatFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/chat/ChatFragment.kt
@@ -4,12 +4,14 @@ import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
 import com.mashup.presentation.common.extension.setThemeContent
 import com.mashup.presentation.databinding.FragmentChatBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Ssam_D_Android
  * @author jaesung
  * @created 2023/06/27
  */
+@AndroidEntryPoint
 class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
     override fun initViews() {
         binding.composeView.setThemeContent {
