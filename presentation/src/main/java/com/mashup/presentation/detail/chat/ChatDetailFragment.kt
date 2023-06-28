502d9fa9bb2a387b3b80f89be049e098c9f1e3c4
JaesungLeee <viki0114@naver.com>
2023-06-29 03:00:26 +0900

    [feat] #92 Fragment에 composeview 연결

--- presentation/src/main/java/com/mashup/presentation/detail/chat/ChatDetailFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/detail/chat/ChatDetailFragment.kt
@@ -2,8 +2,10 @@ package com.mashup.presentation.detail.chat
 
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.common.extension.navigateUp
 import com.mashup.presentation.common.extension.setThemeContent
 import com.mashup.presentation.databinding.FragmentChatDetailBinding
+import com.mashup.presentation.detail.chat.compose.ChatDetailScreen
 
 /**
  * Ssam_D_Android
@@ -13,7 +15,9 @@ import com.mashup.presentation.databinding.FragmentChatDetailBinding
 class ChatDetailFragment : BaseFragment<FragmentChatDetailBinding>(R.layout.fragment_chat_detail) {
     override fun initViews() {
         binding.composeView.setThemeContent {
-
+            ChatDetailScreen(
+                navigateUp = { navigateUp() }
+            )
         }
     }
 }
