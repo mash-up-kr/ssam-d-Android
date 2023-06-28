8a8ba5e054bd4248249a6b195d071b6c4742c892
Huijiny <jiin20803@gmail.com>
2023-06-28 16:31:35 +0900

    [feat] #84 HomeFragment 시그널 recyclerview 초기화

--- presentation/src/main/java/com/mashup/presentation/home/HomeFragment.kt
+++ presentation/src/main/java/com/mashup/presentation/home/HomeFragment.kt
@@ -1,10 +1,11 @@
 package com.mashup.presentation.home
 
+import SimpleGapItemDecorator
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseFragment
-import com.mashup.presentation.common.extension.navigate
-import com.mashup.presentation.common.extension.setThemeContent
 import com.mashup.presentation.databinding.FragmentHomeBinding
+import com.mashup.presentation.home.adapter.SignalAdapter
+import com.mashup.presentation.home.model.SignalUiModel
 
 /**
  * Ssam_D_Android
@@ -12,12 +13,21 @@ import com.mashup.presentation.databinding.FragmentHomeBinding
  * @created 2023/06/04
  */
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val adapter: SignalAdapter by lazy {
+        SignalAdapter()
+    }
 
     override fun initViews() {
-        binding.composeView.setThemeContent {
-            HomeScreen(
-                navigateToSubscribeKeyword = { navigate(R.id.action_home_to_subscribeKeyword)}
+        super.initViews()
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(
+            SimpleGapItemDecorator(
+                gapSize = 12,
+                orientation = SimpleGapDecoratorDirection.VERTICAL
             )
+        )
+    }
         }
     }
 }
