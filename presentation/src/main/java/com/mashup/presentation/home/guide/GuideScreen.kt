d98751072709552bcf360654c78ea4fa290eba54
Huijiny <jiin20803@gmail.com>
2023-06-29 23:37:03 +0900

    [feat] #100 행성 가이드 Screen Composable 생성

--- presentation/src/main/java/com/mashup/presentation/home/guide/GuideScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/home/guide/GuideScreen.kt
@@ -1,3 +1,69 @@
+package com.mashup.presentation.home.guide
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.presentation.R
+import com.mashup.presentation.ui.common.KeyLinkButton
+import com.mashup.presentation.ui.common.KeyLinkToolbar
+import com.mashup.presentation.ui.theme.*
+
+@Composable
+fun PlanetGuideScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold {
+        Image(
+            modifier = modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.img_home_guide),
+            contentDescription = "홈 가이드 백그라운드",
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            KeyLinkToolbar(
+                backgroundColor = Color.Transparent
+            ) {
+                // TODO: 뒤로가기
+            }
+            Column(
+                modifier = modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                PlanetGuide()
+                Spacer(modifier = Modifier.height(72.dp))
+                Aliens()
+                Spacer(modifier = Modifier.height(48.dp))
+                PlanetGuideFooter()
+            }
+        }
+
+    }
+}
 
 @Composable
 fun PlanetGuide() {
@@ -114,4 +180,10 @@ fun PlanetGuideFooter() {
                 })
         KeyLinkButton(modifier = Modifier.fillMaxWidth(), text = "시그널 보내기")
     }
+}
+
+@Preview
+@Composable
+fun PreviewGuideScreen() {
+    PlanetGuideScreen()
 }
