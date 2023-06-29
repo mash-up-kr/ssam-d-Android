a64a5dac9f8d6257b808c9fad5424ea44b2f957b
Huijiny <jiin20803@gmail.com>
2023-06-29 23:34:59 +0900

    [feat] #100 행성 가이드 Composable 생성

--- presentation/src/main/java/com/mashup/presentation/home/guide/GuideScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/home/guide/GuideScreen.kt
@@ -1,5 +1,40 @@
 
 @Composable
+fun PlanetGuide() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "행성 Zylix-6",
+            style = Heading1,
+            color = White
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "곧 다른 행성 이동 가능한 기능이 출시 예정입니다. \n거기는 어떤 종족들이 살고 있을까요?",
+
+            modifier = Modifier
+                .graphicsLayer(alpha = 0.99f)
+                .drawWithCache {
+                    val brush = Brush.horizontalGradient(listOf(Purple, Mint))
+                    onDrawWithContent {
+                        drawContent()
+                        drawRect(brush, blendMode = BlendMode.SrcAtop)
+                    }
+                },
+            textAlign = TextAlign.Center,
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = "Zylix-6의 종족들은 상호 간의 연결성과 유대감을 중요시합니다. 그들은 공동체 의식을 갖고 있으며, 협력과 상호작용을 통해 문화적인 발전과 지식의 공유를 추구합니다. 이를 통해 행성 전체의 번영을 이루는데 기여합니다.",
+            style = Body1,
+            color = Gray07,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
 fun Aliens() {
     Column {
         AlienInfo()
