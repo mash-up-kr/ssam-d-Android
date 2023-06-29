557418085c0ee9d28a303d1ae56d6a857ebc1833
Huijiny <jiin20803@gmail.com>
2023-06-30 00:07:39 +0900

    [feat] #100 GuideFragment 추가 및 네비게이션 정의

---
+++ presentation/src/main/java/com/mashup/presentation/home/guide/GuideFragment.kt
@@ -0,0 +1,14 @@
+package com.mashup.presentation.home.guide
+
+import com.mashup.presentation.R
+import com.mashup.presentation.common.base.BaseFragment
+import com.mashup.presentation.common.extension.setThemeContent
+import com.mashup.presentation.databinding.FragmentGuideBinding
+
+class GuideFragment: BaseFragment<FragmentGuideBinding>(R.layout.fragment_guide) {
+    override fun initViews() {
+        binding.composeView.setThemeContent {
+            PlanetGuideScreen()
+        }
+    }
+}
