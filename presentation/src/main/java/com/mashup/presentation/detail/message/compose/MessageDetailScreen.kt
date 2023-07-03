fd332b716628a47f804585e8b4b23a34c7b47ff1
Jaesung Lee <viki0114@naver.com>
2023-07-03 21:12:15 +0900

    [feat] #108 메시지 상세 내용 및 키워드 컴포넌트 추가

--- presentation/src/main/java/com/mashup/presentation/detail/message/compose/MessageDetailScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/detail/message/compose/MessageDetailScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.presentation.R
 import com.mashup.presentation.ui.common.KeyLinkToolbar
+import com.mashup.presentation.ui.common.KeywordChip
 import com.mashup.presentation.ui.theme.*
 
 /**
@@ -27,6 +28,8 @@ import com.mashup.presentation.ui.theme.*
 fun MessageDetailScreen(
     modifier: Modifier = Modifier
 ) {
+    val matchedKeywords = listOf("매쉬업", "일상", "디자인", "IT", "취준", "일상", "디자인", "IT", "취준")
+
     Scaffold(
         modifier = modifier,
         backgroundColor = Black,
@@ -55,7 +58,12 @@ fun MessageDetailScreen(
         ) {
             MessageInfo(modifier = Modifier, othersName = "연날리기", date = "2023년 5월 30일")
 
+            MessageDetailContent(modifier = Modifier.padding(top = 16.dp))
 
+            MatchedKeywordContent(
+                modifier = Modifier.padding(top = 52.dp),
+                matchedKeywords = matchedKeywords
+            )
         }
     }
 
@@ -98,6 +106,106 @@ fun MessageInfo(
                 style = Caption1,
                 color = Gray06
             )
+        }
+    }
+}
+
+@Composable
+fun MessageDetailContent(
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier,
+        text = "이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였죠? 이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였죠?이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였죠?이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였죠?이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였죠?이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였죠?이번주 불참해서 공지를 못 들음...다음 전체회의에 준비할 내용이 어떤거였",
+        style = Body1,
+        color = Gray10
+    )
+}
+
+@Composable
+fun MatchedKeywordContent(
+    matchedKeywords: List<String>,
+    modifier: Modifier = Modifier
+) {
+    MatchedKeywordHeader(
+        modifier = modifier,
+        matchedKeywords = matchedKeywords.size
+    )
+    MatchedKeywords(matchedKeywords = matchedKeywords)
+}
+
+@Composable
+fun MatchedKeywordHeader(
+    matchedKeywords: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_frequency_mint_16),
+            tint = Mint,
+            contentDescription = stringResource(id = R.string.content_description_chat_item_frequency)
+        )
+
+        Text(
+            modifier = Modifier.padding(start = 4.dp),
+            text = stringResource(
+                id = R.string.message_detail_matched_signal_count,
+                matchedKeywords
+            ),
+            style = Title2,
+            color = Mint
+        )
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun MatchedKeywords(
+    matchedKeywords: List<String>,
+    modifier: Modifier = Modifier,
+) {
+    FlowRow(
+        modifier = Modifier.padding(top = 20.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        matchedKeywords.forEach { keyword ->
+            KeywordChip(
+                keyword = keyword,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MessageScreenPreview() {
+    SsamDTheme {
+        MessageDetailScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MessageInfoPreview() {
+    SsamDTheme {
+        Surface(color = Black) {
+            MessageInfo(othersName = "연날리기", date = "2023년 5월 30일")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MatchedKeywordContentPreview() {
+    val matchedKeywords = listOf("매쉬업", "일상", "디자인", "IT", "취준", "일상", "디자인", "IT", "취준")
+    SsamDTheme {
+        Surface(color = Black) {
+            MatchedKeywordContent(matchedKeywords)
         }
     }
 }
