99c99b5d2b152acf782de6a544850ad40b2bf8cd
sooziini <soojinpar1026@gmail.com>
2023-07-03 22:25:03 +0900

    [feat] #109 patchNickname api 추가

--- data/src/main/java/com/mashup/data/source/remote/service/LoginService.kt
+++ data/src/main/java/com/mashup/data/source/remote/service/LoginService.kt
@@ -4,7 +4,9 @@ import com.mashup.data.source.remote.dto.BaseResponse
 import com.mashup.data.source.remote.dto.requestbody.LoginRequestBody
 import com.mashup.data.source.remote.dto.responsebody.LoginResponseBody
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface LoginService {
 
@@ -12,4 +14,9 @@ interface LoginService {
     suspend fun login(
         @Body requestBody: LoginRequestBody
     ): BaseResponse<LoginResponseBody>
+
+    @PATCH("users/nickname")
+    suspend fun patchNickname(
+        @Query("nickname") nickname: String
+    ): BaseResponse<Unit>
 }
