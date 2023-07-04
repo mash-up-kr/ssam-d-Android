edcb5adbacf2d5228912e98f2a6d180ac1e0f577
sooziini <soojinpar1026@gmail.com>
2023-07-04 22:41:57 +0900

    [feat] #109 @Query -> @Field 수정

--- data/src/main/java/com/mashup/data/source/remote/service/LoginService.kt
+++ data/src/main/java/com/mashup/data/source/remote/service/LoginService.kt
@@ -4,9 +4,10 @@ import com.mashup.data.source.remote.dto.BaseResponse
 import com.mashup.data.source.remote.dto.requestbody.LoginRequestBody
 import com.mashup.data.source.remote.dto.responsebody.LoginResponseBody
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface LoginService {
 
@@ -15,8 +16,9 @@ interface LoginService {
         @Body requestBody: LoginRequestBody
     ): BaseResponse<LoginResponseBody>
 
+    @FormUrlEncoded
     @PATCH("users/nickname")
     suspend fun patchNickname(
-        @Query("nickname") nickname: String
-    ): BaseResponse<Unit>
+        @Field("nickname") nickname: String
+    ): BaseResponse<Any>
 }
