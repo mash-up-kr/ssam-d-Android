16f0ab2a513c03e6554c98721b7ff3adee04451f
Jaesung Lee <viki0114@naver.com>
2023-07-04 17:49:32 +0900

    [feat] #111 Home화면 route 및 graph 생성

---
+++ presentation/src/main/java/com/mashup/presentation/navigation/KeyLinkNavigationRoute.kt
@@ -0,0 +1,15 @@
+package com.mashup.presentation.navigation
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/04
+ */
+sealed class KeyLinkNavigationRoute(val route: String) {
+    object HomeGraph : KeyLinkNavigationRoute(homeGraphPattern) {
+        object HomeRoute : KeyLinkNavigationRoute(homeRoute)
+        object EditKeywordRoute : KeyLinkNavigationRoute(editKeywordRoute)
+        object GuideRoute : KeyLinkNavigationRoute(guideRoute)
+        object ProfileRoute : KeyLinkNavigationRoute(profileRoute)
+    }
+}
