0d3dc3ab43e07c72f56944e0935a5f3427895f1f
Jaesung Lee <viki0114@naver.com>
2023-07-04 17:30:45 +0900

    [feat] #111 KeyLinkApp 및 KeyLinkAppState 추가 * bottomBar * snackbarhost에 KeyLinkSnackbar 연결 * NavHost * appState

---
+++ presentation/src/main/java/com/mashup/presentation/KeyLinkApp.kt
@@ -0,0 +1,53 @@
+package com.mashup.presentation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.SnackbarResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.mashup.presentation.navigation.KeyLinkNavHost
+import com.mashup.presentation.ui.common.KeyLinkSnackBar
+import com.mashup.presentation.ui.theme.Black
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/04
+ */
+@Composable
+fun KeyLinkApp(
+    appState: KeyLinkAppState = rememberKeyLinkAppState()
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        bottomBar = {
+            if (appState.isBottomBarVisible()) {
+                KeyLinkBottomNavigationBar()
+            }
+        },
+        backgroundColor = Black,
+        snackbarHost = { KeyLinkSnackBar(snackBarHostState = snackbarHostState) }
+    ) { innerPadding ->
+        KeyLinkNavHost(
+            appState = appState,
+            startDestination = "",
+            modifier = Modifier.padding(innerPadding),
+            onShowSnackbar = { message, action ->
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    actionLabel = action,
+                    duration = SnackbarDuration.Short
+                ) == SnackbarResult.ActionPerformed
+            }
+        )
+    }
+}
+
+@Composable
+fun KeyLinkBottomNavigationBar() {
+
+}
