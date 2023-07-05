45bf8197e959cd38a8cd355c3ada317af5ea328e
Jaesung Lee <viki0114@naver.com>
2023-07-05 16:02:21 +0900

    [feat] #62 API 주소를 보관하는 클래스 생성

---
+++ data/src/main/java/com/mashup/data/source/remote/service/ApiPattern.kt
@@ -0,0 +1,13 @@
+package com.mashup.data.source.remote.service
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/05
+ */
+object ApiPattern {
+    object Keywords {
+        const val prefix = "keywords/"
+        const val recommend = prefix + "recommend"
+    }
+}
