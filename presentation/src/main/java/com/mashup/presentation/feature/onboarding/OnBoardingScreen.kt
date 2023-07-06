03c49c09928020d687c55af2eb5440605bb6cb23
Huijiny <jiin20803@gmail.com>
2023-07-07 00:48:31 +0900

    [refact] #113 미사용 코드제거

--- presentation/src/main/java/com/mashup/presentation/feature/onboarding/OnBoardingScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/onboarding/OnBoardingScreen.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -116,32 +114,6 @@ fun OnBoardingContent(
                 saveKeywords(keywords)
             },
             enable = keywords.isNotEmpty()
-        )
-    }
-}
-
-
-@Composable
-fun NicknameScreen() {
-    var nickname by remember { mutableStateOf("") }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Spacer(modifier = Modifier.height(12.dp))
-        KeyLinkMintText(text = "사용할 닉네임을 입력해주세요")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "10글자 이내", fontSize = 14.sp, color = Gray07)
-        Spacer(modifier = Modifier.height(56.dp))
-        KeyLinkOnBoardingTextField(
-            value = nickname,
-            onValueChange = { nickname = it },
-            hint = "닉네임 입력",
-            fontSize = 32.sp,
-            onClickDone = { /* 키보드 닫기 */ },
-            maxLength = 10
         )
     }
 }
