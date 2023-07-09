8bccc0ec97d4129892380a2041da69f73286171d
Jaesung Lee <viki0114@naver.com>
2023-07-09 23:33:20 +0900

    [feat] #111 ChatDetailRoute 연결 및 기존 Fragment 제거

---
+++ presentation/src/main/java/com/mashup/presentation/feature/detail/chat/navigation/ChatDetailNavigation.kt
@@ -0,0 +1,18 @@
+package com.mashup.presentation.feature.detail.chat.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import com.mashup.presentation.navigation.KeyLinkNavigationRoute
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/09
+ */
+
+fun NavController.navigateToChatDetail(navOptions: NavOptions? = null) {
+    navigate(
+        route = KeyLinkNavigationRoute.ChatGraph.ChatDetailRoute.route,
+        navOptions = navOptions
+    )
+}
