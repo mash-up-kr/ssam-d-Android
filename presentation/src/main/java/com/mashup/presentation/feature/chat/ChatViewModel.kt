4864f867fbf2cd70091851d8182802e1997a73a8
sooziini <soojinpar1026@gmail.com>
2023-07-09 18:44:10 +0900

    [feat] #123 ChatViewModel 생성 및 screen 연결

--- presentation/src/main/java/com/mashup/presentation/feature/chat/ChatViewModel.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/chat/ChatViewModel.kt
@@ -1,7 +1,10 @@
 package com.mashup.presentation.feature.chat
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.domain.usecase.GetChatsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -10,5 +13,15 @@ import javax.inject.Inject
  * @created 2023/07/09
  */
 @HiltViewModel
-class ChatViewModel @Inject constructor() : ViewModel() {
+class ChatViewModel @Inject constructor(
+    private val getChatsUseCase: GetChatsUseCase
+): ViewModel() {
+
+    fun getChats(id: Int) {
+        viewModelScope.launch {
+            getChatsUseCase.execute(id)
+                .onSuccess {  }
+                .onFailure {  }
+        }
+    }
 }
