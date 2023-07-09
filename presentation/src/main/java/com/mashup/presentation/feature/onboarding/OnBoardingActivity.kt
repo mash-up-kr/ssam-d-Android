abbde121e29319fdf5e25c003c297b9954ca6660
Jaesung Lee <viki0114@naver.com>
2023-07-09 22:44:53 +0900

    [fix] #111 Binding 에러 수정

--- presentation/src/main/java/com/mashup/presentation/feature/onboarding/OnBoardingActivity.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/onboarding/OnBoardingActivity.kt
@@ -2,8 +2,8 @@ package com.mashup.presentation.feature.onboarding
 
 import com.mashup.presentation.R
 import com.mashup.presentation.common.base.BaseActivity
-import com.mashup.presentation.databinding.ActivitySignalBinding
+import com.mashup.presentation.databinding.ActivityOnboardingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OnBoardingActivity : BaseActivity<ActivitySignalBinding>(R.layout.activity_onboarding)
+class OnBoardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding)
