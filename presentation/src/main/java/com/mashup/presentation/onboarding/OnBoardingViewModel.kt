fad85bc95b27c18815b9bc341eaeb09d6ef1fdb0
Huijiny <jiin20803@gmail.com>
2023-07-09 23:37:37 +0900

    [refact] #103 uiEvent 제거 및 uiState 하나로 통일

--- presentation/src/main/java/com/mashup/presentation/onboarding/OnBoardingViewModel.kt
+++ presentation/src/main/java/com/mashup/presentation/onboarding/OnBoardingViewModel.kt
@@ -6,43 +6,58 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mashup.domain.usecase.SaveOnboardingKeywordsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
     private val saveOnboardingKeywordsUseCase: SaveOnboardingKeywordsUseCase
-): ViewModel() {
-    val uiEvent: MutableSharedFlow<UiEvent> = MutableSharedFlow()
-    var keywords = mutableStateListOf<String>()
-        private set
+) : ViewModel() {
+    val uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState.Editing(emptyList()))
 
-
-    suspend fun saveKeywords(keywords:List<String>) {
+    fun saveKeywords(keywords: List<String>) {
         viewModelScope.launch {
+            uiState.emit(UiState.Loading)
             saveOnboardingKeywordsUseCase.execute(keywords)
                 .onSuccess {
-                    uiEvent.emit(UiEvent.SuccessSave)
+                    uiState.emit(UiState.SaveSuccess)
                 }
                 .onFailure {
                     it.message?.let {
-                        uiEvent.emit(UiEvent.ShowError(it))
+                        uiState.emit(UiState.SaveFailed(it))
                     }
                 }
         }
     }
 
     fun addKeyword(keyword: String) {
-        keywords.add(keyword)
+        if (uiState.value is UiState.Editing) {
+            val keywords = (uiState.value as UiState.Editing).keywords.toMutableList()
+
+            keywords.add(keyword)
+            viewModelScope.launch {
+                uiState.emit(UiState.Editing(keywords.toList()))
+            }
+        }
     }
 
     fun removeKeyword(index: Int) {
-        keywords.removeAt(index)
+        if (uiState.value is UiState.Editing) {
+            val keywords = (uiState.value as UiState.Editing).keywords.toMutableList()
+
+            keywords.removeAt(index)
+            viewModelScope.launch {
+                uiState.emit(UiState.Editing(keywords.toList()))
+            }
+        }
     }
 
-    sealed class UiEvent {
-        object SuccessSave: UiEvent()
-        class ShowError(val message: String): UiEvent()
+    sealed class UiState {
+        object Loading : UiState()
+        object SaveSuccess : UiState()
+        data class SaveFailed(val message: String) : UiState()
+
+        data class Editing(val keywords: List<String>) : UiState()
     }
 }
