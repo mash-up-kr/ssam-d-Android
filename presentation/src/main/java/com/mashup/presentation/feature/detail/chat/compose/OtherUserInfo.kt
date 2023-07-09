9fb7b9cdfc054ec065840b739a852d8042c601b1
Huijiny <jiin20803@gmail.com>
2023-07-10 00:12:33 +0900

    [feat] #125 채팅 핑퐁 창 유저 정보 배치 변경

--- presentation/src/main/java/com/mashup/presentation/feature/detail/chat/compose/OtherUserInfo.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/detail/chat/compose/OtherUserInfo.kt
@@ -1,15 +1,13 @@
 package com.mashup.presentation.feature.detail.chat.compose
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.presentation.ui.theme.Red
@@ -29,9 +27,10 @@ fun OtherUserInfo(
     othersProfileImage: String = "",
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier.padding(vertical = 8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Row(
+        modifier = modifier.padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Box(
             modifier = Modifier
@@ -39,14 +38,13 @@ fun OtherUserInfo(
                 .background(
                     color = Red,
                     shape = CircleShape
-                ),
+                )
         )
 
         Text(
             text = othersNickName,
             style = Title2,
-            color = White,
-            modifier = Modifier.padding(top = 8.dp)
+            color = White
         )
     }
 }
