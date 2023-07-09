151b72b0e8f65151073f7cc1107a5ff469b42f2f
Huijiny <jiin20803@gmail.com>
2023-07-09 23:53:50 +0900

    [refact] #103 미사용 파일 제거

--- presentation/src/main/java/com/mashup/presentation/onboarding/OnBoardingScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/onboarding/OnBoardingScreen.kt
@@ -1,33 +1,21 @@
 package com.mashup.presentation.onboarding
 
 import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mashup.presentation.R
 import com.mashup.presentation.ui.common.*
-import com.mashup.presentation.ui.theme.Gray02
 import com.mashup.presentation.ui.theme.Gray07
 import com.mashup.presentation.ui.theme.SsamDTheme
-import com.mashup.presentation.ui.theme.White
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 
 @Composable
@@ -36,7 +24,6 @@ fun OnBoardingScreen(
     navigateToNotificationPermission: () -> Unit,
     viewModel: OnBoardingViewModel = hiltViewModel()
 ) {
-    val coroutineScope = rememberCoroutineScope()
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Column(modifier = modifier.fillMaxSize()) {
@@ -59,19 +46,6 @@ fun OnBoardingScreen(
             }
             is OnBoardingViewModel.UiState.SaveFailed -> {
                 // TODO: show error Snackbar
-            }
-        }
-    }
-
-    LaunchedEffect(key1 = uiState) {
-        coroutineScope.launch {
-            viewModel.uiState.collectLatest {
-                when (it) {
-                    is OnBoardingViewModel.UiState.SaveSuccess -> {
-                        navigateToNotificationPermission()
-                    }
-                    else -> {}
-                }
             }
         }
     }
