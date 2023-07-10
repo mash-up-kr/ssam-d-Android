7654989dde050cbf2375d2971bbfec9b224af19b
Jaesung Lee <viki0114@naver.com>
2023-07-11 01:39:46 +0900

    [feat] #128 답장 UI 디자인

---
+++ presentation/src/main/java/com/mashup/presentation/feature/reply/ReplyScreen.kt
@@ -0,0 +1,173 @@
+package com.mashup.presentation.feature.reply
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.presentation.R
+import com.mashup.presentation.ui.common.*
+import com.mashup.presentation.ui.theme.Black
+import com.mashup.presentation.ui.theme.Body2
+import com.mashup.presentation.ui.theme.SsamDTheme
+import com.mashup.presentation.ui.theme.White
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/10
+ */
+
+/**
+ * 임시 뷰모델
+ */
+@HiltViewModel
+class SampleViewModel @Inject constructor() : ViewModel() {
+    fun sendReply(reply: String) {
+        viewModelScope.launch {
+            // Call UseCase
+        }
+    }
+}
+
+@Composable
+fun ReplyRoute(
+    onClickBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: SampleViewModel = hiltViewModel()
+) {
+    var reply by rememberSaveable { mutableStateOf("") }
+
+    ReplyScreen(
+        reply = reply,
+        modifier = modifier,
+        onReplyTextChange = { reply = it },
+        onClickBack = onClickBack,
+        onSendClick = { viewModel.sendReply(reply) }
+    )
+}
+
+@Composable
+private fun ReplyScreen(
+    reply: String,
+    onReplyTextChange: (String) -> Unit,
+    onClickBack: () -> Unit,
+    onSendClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var showGoBackDialog by rememberSaveable { mutableStateOf(false) }
+    var showLengthOverDialog by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = modifier,
+        backgroundColor = Black,
+        topBar = {
+            KeyLinkToolbar(
+                title = {
+                    Text(
+                        text = "시그널 답장",
+                        style = Body2,
+                        color = White
+                    )
+                },
+                onClickBack = { showGoBackDialog = true }
+            )
+        }
+    ) { innerPaddingValues ->
+        ReplyContent(
+            modifier = Modifier.padding(innerPaddingValues),
+            reply = reply,
+            onReplyTextChange = onReplyTextChange,
+            onSendClick = onSendClick,
+            onLengthOver = { showLengthOverDialog = true }
+        )
+
+        if (showGoBackDialog) {
+            KeyLinkGoBackDialog(
+                onDismissRequest = { showGoBackDialog = false },
+                onCloseClick = { showGoBackDialog = false },
+                onGoBackClick = onClickBack
+            )
+        }
+
+        if (showLengthOverDialog) {
+            KeyLinkContentLengthDialog(
+                onDismissRequest = { showLengthOverDialog = false },
+                onButtonClick = { showLengthOverDialog = false }
+            )
+        }
+    }
+}
+
+@Composable
+fun ReplyContent(
+    reply: String,
+    onReplyTextChange: (String) -> Unit,
+    onSendClick: () -> Unit,
+    onLengthOver: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        KeyLinkTextField(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+            value = reply,
+            onValueChange = { reply ->
+                if (reply.length >= 300) onLengthOver()
+                else onReplyTextChange(reply)
+            },
+            hint = stringResource(id = R.string.hint_signal_content),
+            hintAlign = TextAlign.Start,
+            onClickDone = { /*TODO*/ },
+            fontSize = 18.sp,
+            maxLength = 300
+        )
+        KeyLinkButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp, horizontal = 20.dp)
+                .padding(bottom = 48.dp),
+            text = stringResource(id = R.string.next),
+            onClick = onSendClick,
+            enable = reply.isNotEmpty()
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ReplyScreenPreview() {
+    SsamDTheme {
+        ReplyRoute(
+            onClickBack = {},
+        )
+    }
+}
