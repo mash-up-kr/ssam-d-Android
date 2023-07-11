be2c57e71fc1a1f71518f22cbf4536ee9d964ac6
Jaesung Lee <viki0114@naver.com>
2023-07-11 21:28:35 +0900

    [feat] #130 ProfileViewModel 생성

--- presentation/src/main/java/com/mashup/presentation/feature/profile/ProfileViewModel.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/profile/ProfileViewModel.kt
@@ -1,7 +1,12 @@
 package com.mashup.presentation.feature.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -10,5 +15,22 @@ import javax.inject.Inject
  * @created 2023/07/11
  */
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val optionsProvider: ProfileOptionsProvider
+) : ViewModel() {
+
+    private val _optionsList: MutableStateFlow<List<ProfileViewType>> = MutableStateFlow(emptyList())
+    val optionsList: StateFlow<List<ProfileViewType>> = _optionsList.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _optionsList.value = optionsProvider.getOptions()
+        }
+    }
+
+    fun toggleNotificationSwitch(isChecked: Boolean) {
+        viewModelScope.launch {
+
+        }
+    }
 }
