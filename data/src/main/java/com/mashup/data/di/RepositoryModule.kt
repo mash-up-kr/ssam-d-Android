2dcae6f5bd97c90161aa11ca28b620906a842b2a
Jaesung Lee <viki0114@naver.com>
2023-07-14 02:12:02 +0900

    [feat] #62 Repository 생성 및 바인딩 모듈 추가

--- data/src/main/java/com/mashup/data/di/RepositoryModule.kt
+++ data/src/main/java/com/mashup/data/di/RepositoryModule.kt
@@ -3,9 +3,11 @@ package com.mashup.data.di
 import com.mashup.data.repository.KeywordRepositoryImpl
 import com.mashup.data.repository.LoginRepositoryImpl
 import com.mashup.data.repository.OnboardingRepositoryImpl
+import com.mashup.data.repository.SignalRepositoryImpl
 import com.mashup.domain.repository.KeywordRepository
 import com.mashup.domain.repository.LoginRepository
 import com.mashup.domain.repository.OnboardingRepository
+import com.mashup.domain.repository.SignalRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -27,4 +29,8 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindKeywordRepository(keywordRepositoryImpl: KeywordRepositoryImpl): KeywordRepository
+
+    @Binds
+    @Singleton
+    fun bindSignalRepository(signalRepositoryImpl: SignalRepositoryImpl): SignalRepository
 }
