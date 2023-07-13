035578a40ab5a8e5e7f2b8ab5d1f334b09b8b0fe
Huijiny <jiin20803@gmail.com>
2023-07-14 00:58:23 +0900

    [feat] #135 User domainModel 생성

---
+++ domain/src/main/java/com/mashup/domain/model/User.kt
@@ -0,0 +1,12 @@
+package com.mashup.domain.model
+
+import com.mashup.domain.base.DomainModel
+
+data class User(
+    val id: Long,
+    val nickname: String,
+    val email: String,
+    val provider: String,
+    val profileImageUrl: String,
+    val agreeAlarm: Boolean
+): DomainModel
