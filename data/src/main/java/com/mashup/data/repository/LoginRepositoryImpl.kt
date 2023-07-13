acc620579b1368d291549b9b379f10e464620074
Huijiny <jiin20803@gmail.com>
2023-07-14 00:58:48 +0900

    [feat] #135 마이페이지 api repository 로직

--- data/src/main/java/com/mashup/data/repository/LoginRepositoryImpl.kt
+++ data/src/main/java/com/mashup/data/repository/LoginRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.mashup.data.repository
 import com.mashup.data.source.local.datasource.LocalLoginDataSource
 import com.mashup.data.source.remote.datasource.RemoteLoginDataSource
 import com.mashup.data.source.remote.dto.requestbody.LoginRequestBody
+import com.mashup.domain.model.User
 import com.mashup.domain.repository.LoginRepository
 import com.mashup.domain.usecase.LoginParam
 import javax.inject.Inject
@@ -10,7 +11,7 @@ import javax.inject.Inject
 class LoginRepositoryImpl @Inject constructor(
     private val remoteLoginDataSource: RemoteLoginDataSource,
     private val localLoginDataSource: LocalLoginDataSource
-): LoginRepository {
+) : LoginRepository {
 
     override suspend fun login(param: LoginParam): Boolean {
         val loginRequestBody = with(param) {
@@ -32,5 +33,19 @@ class LoginRepositoryImpl @Inject constructor(
 
     override suspend fun patchNickname(nickname: String) {
         remoteLoginDataSource.patchNickname(nickname)
+    }
+
+    override suspend fun patchAlarm(isAgree: Boolean) {
+        remoteLoginDataSource.patchAlarm(isAgree)
+    }
+
+    override suspend fun getUser(id: Long): User {
+        remoteLoginDataSource.getUser(id).let {result ->
+            return result.data?.toDomainModel() ?: throw Exception(result.message)
+        }
+    }
+
+    override suspend fun deleteUser(id: Long) {
+        remoteLoginDataSource.deleteUser(id)
     }
 }
--- domain/src/main/java/com/mashup/domain/repository/LoginRepository.kt
+++ domain/src/main/java/com/mashup/domain/repository/LoginRepository.kt
@@ -1,8 +1,15 @@
 package com.mashup.domain.repository
 
+import com.mashup.domain.model.User
 import com.mashup.domain.usecase.LoginParam
 
 interface LoginRepository {
     suspend fun login(param: LoginParam): Boolean
     suspend fun patchNickname(nickname: String)
+
+    suspend fun patchAlarm(isAgree: Boolean)
+
+    suspend fun getUser(id: Long): User
+
+    suspend fun deleteUser(id: Long)
 }
