f723f2817c51b0fa809471fa1652eb9b185a7874
sooziini <soojinpar1026@gmail.com>
2023-07-14 15:08:47 +0900

    [feat] #138 user id 및 login 완료 여부 LocalUserDataStore에서 관리할 수 있도록 세팅

--- data/src/main/java/com/mashup/data/source/local/datasource/LocalUserDataSource.kt
+++ data/src/main/java/com/mashup/data/source/local/datasource/LocalUserDataSource.kt
@@ -1,13 +1,42 @@
 package com.mashup.data.source.local.datasource
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import com.mashup.data.network.AppHeaderProvider
 import javax.inject.Inject
 
 class LocalUserDataSource @Inject constructor(
-    private val appHeaderProvider: AppHeaderProvider
+    private val appHeaderProvider: AppHeaderProvider,
+    application: Application
 ) {
+    private val preferences: SharedPreferences =
+        application.getSharedPreferences(LOGIN_PREFERENCE, Context.MODE_PRIVATE)
 
     fun saveToken(token: String?) {
         appHeaderProvider.saveToken(token)
+    }
+
+    fun getUserId(): Long {
+        return preferences.getLong(USER_ID, -1)
+    }
+
+    fun setUserId(id: Long) {
+        preferences.edit().putLong(USER_ID, id).apply()
+    }
+
+    fun getIsLoginCompleted(): Boolean {
+        return preferences.getBoolean(IS_LOGIN_COMPLETED, false)
+    }
+
+    fun setIsLoginCompleted(isLoginCompleted: Boolean) {
+        preferences.edit().putBoolean(IS_LOGIN_COMPLETED, isLoginCompleted).apply()
+    }
+
+    companion object {
+        private const val LOGIN_PREFERENCE = "LOGIN_PREFERENCE"
+        private const val JWT = "JWT"
+        private const val USER_ID = "USER_ID"
+        private const val IS_LOGIN_COMPLETED = "IS_LOGIN_COMPLETED"
     }
 }
