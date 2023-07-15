d2a78fad6accae3a149da40aea3acebc1215e1b0
sooziini <soojinpar1026@gmail.com>
2023-07-15 15:18:43 +0900

    [fix] #138 중복 함수 제거

--- domain/src/main/java/com/mashup/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/mashup/domain/repository/UserRepository.kt
@@ -3,8 +3,6 @@ package com.mashup.domain.repository
 import com.mashup.domain.usecase.LoginParam
 
 interface UserRepository {
-    suspend fun login(param: LoginParam): Boolean
-
     suspend fun login(param: LoginParam)
 
     suspend fun getNicknameDuplication(nickname: String): Result<Unit>
