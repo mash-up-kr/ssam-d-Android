0c7e3c82a0b47990a14ac9ce0e88150405c93f4e
Huijiny <jiin20803@gmail.com>
2023-07-15 15:52:20 +0900

    [feat] #143 알림설정화면 스케일 crop

--- presentation/src/main/java/com/mashup/presentation/feature/onboarding/NotificationPermissionScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/onboarding/NotificationPermissionScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -98,12 +99,13 @@ fun NotificationPermissionGuideImage(
 ) {
     Box(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(top = 48.dp),
+            .padding(top = 48.dp)
     ) {
         Image(
+            modifier = modifier.fillMaxWidth(),
             painter = painterResource(id = R.drawable.img_notificationsenabled),
-            contentDescription = stringResource(id = R.string.notification_img_content_description)
+            contentDescription = stringResource(id = R.string.notification_img_content_description),
+            contentScale = ContentScale.Crop
         )
     }
 }
