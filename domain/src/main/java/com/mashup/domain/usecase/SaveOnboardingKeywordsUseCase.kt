35a9d3fcc93e53ee3c70e774c690b74ce56f0ae1
Jaesung Lee <viki0114@naver.com>
2023-07-17 14:40:03 +0900

    [style] #153 BaseUseCase 네이밍 수정 및 패키지 이동

--- domain/src/main/java/com/mashup/domain/usecase/SaveOnboardingKeywordsUseCase.kt
+++ domain/src/main/java/com/mashup/domain/usecase/SaveOnboardingKeywordsUseCase.kt
@@ -1,11 +1,12 @@
 package com.mashup.domain.usecase
 
 import com.mashup.domain.repository.OnboardingRepository
+import com.mashup.domain.usecase.common.CoroutineUseCase
 import javax.inject.Inject
 
 class SaveOnboardingKeywordsUseCase @Inject constructor(
     private val onboardingRepository: OnboardingRepository
-) : BaseUseCase<List<String>, Result<Unit>>() {
+) : CoroutineUseCase<List<String>, Result<Unit>>() {
 
     override suspend fun invoke(param: List<String>): Result<Unit> {
         return runCatching {
--- domain/src/main/java/com/mashup/domain/usecase/mypage/GetUserInformationUseCase.kt
+++ domain/src/main/java/com/mashup/domain/usecase/mypage/GetUserInformationUseCase.kt
@@ -2,12 +2,12 @@ package com.mashup.domain.usecase.mypage
 
 import com.mashup.domain.model.User
 import com.mashup.domain.repository.UserRepository
-import com.mashup.domain.usecase.BaseUseCase
+import com.mashup.domain.usecase.common.CoroutineUseCase
 import javax.inject.Inject
 
 class GetUserInformationUseCase @Inject constructor(
     private val userRepository: UserRepository
-): BaseUseCase<Unit, Result<User>>() {
+): CoroutineUseCase<Unit, Result<User>>() {
     override suspend fun invoke(param: Unit): Result<User> {
         return runCatching {
             userRepository.getUser()
