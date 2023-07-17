4b1290f6582fa82ea8571ad9ca8638db5c520074
Jaesung Lee <viki0114@naver.com>
2023-07-17 14:50:42 +0900

    [feat] #153 UiModel 정의 및 ViewModel 생성

---
+++ presentation/src/main/java/com/mashup/presentation/feature/home/model/SubscribeKeywordUiModel.kt
@@ -0,0 +1,10 @@
+package com.mashup.presentation.feature.home.model
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/17
+ */
+data class SubscribeKeywordUiModel(
+    val a: String
+)
