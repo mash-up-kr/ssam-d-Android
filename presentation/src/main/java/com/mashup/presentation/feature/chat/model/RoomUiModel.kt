7b54d60e55f56a6cdc865ffb360d2149d591377a
Huijiny <jiin20803@gmail.com>
2023-07-18 17:37:55 +0900

    [feat] #146 room uimodel 생성

---
+++ presentation/src/main/java/com/mashup/presentation/feature/chat/model/RoomUiModel.kt
@@ -0,0 +1,28 @@
+package com.mashup.presentation.feature.chat.model
+
+import com.mashup.domain.model.chat.Room
+import com.mashup.presentation.common.extension.getDisplayedTime
+
+data class RoomUiModel (
+    val id: Long,
+    val keywords: List<String>,
+    val recentSignalContent: String,
+    val matchingKeywordCount: Int,
+    val profileImage: String,
+    val receivedTime: String
+) {
+    companion object {
+        fun fromDomainModel(domain: Room): RoomUiModel {
+            return with(domain) {
+                RoomUiModel(
+                    id = id,
+                    keywords = keywords,
+                    recentSignalContent = recentSignalContent,
+                    matchingKeywordCount = matchingKeywordCount,
+                    profileImage = profileImage,
+                    receivedTime = recentSignalReceivedTimeMillis.getDisplayedTime()
+                )
+            }
+        }
+    }
+}
