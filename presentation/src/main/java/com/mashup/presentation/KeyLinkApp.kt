7e4cdb21335f0d9105e85ed581be464e76333aac
Jaesung Lee <viki0114@naver.com>
2023-07-18 22:15:04 +0900

    [style] #153 리뷰반영 - 함수네이밍 수정

--- presentation/src/main/java/com/mashup/presentation/KeyLinkApp.kt
+++ presentation/src/main/java/com/mashup/presentation/KeyLinkApp.kt
@@ -2,11 +2,7 @@ package com.mashup.presentation
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarDuration
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.SnackbarResult
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.mashup.presentation.navigation.KeyLinkNavHost
 import com.mashup.presentation.ui.common.KeyLinkBottomBar
