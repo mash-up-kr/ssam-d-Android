c9aabfaaaa66854c92301a62e3b8dc198c78106f
Jaesung Lee <viki0114@naver.com>
2023-07-19 02:35:27 +0900

    [feat] #149 구독중인 키워드 조회 UseCase 생성

---
+++ domain/src/main/java/com/mashup/domain/usecase/GetSubscribeKeywordsUseCase.kt
@@ -0,0 +1,22 @@
+package com.mashup.domain.usecase
+
+import com.mashup.domain.repository.KeywordRepository
+import com.mashup.domain.usecase.common.FlowUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/19
+ */
+class GetSubscribeKeywordsUseCase @Inject constructor(
+    private val keywordRepository: KeywordRepository
+) : FlowUseCase<Unit, List<String>>() {
+    override fun invoke(params: Unit): Flow<List<String>> = flow {
+        keywordRepository.getSubscribeKeywords().collect { subscribeKeywords ->
+            emit(subscribeKeywords)
+        }
+    }
+}
