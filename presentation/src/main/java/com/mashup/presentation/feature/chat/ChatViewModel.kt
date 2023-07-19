e98e83c7e74b63e054e37fb26017fbd4b67dd618
Huijiny <jiin20803@gmail.com>
2023-07-19 17:43:16 +0900

    [feat] #146 뷰모델 로직 생성

--- presentation/src/main/java/com/mashup/presentation/feature/chat/ChatViewModel.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/chat/ChatViewModel.kt
@@ -1,12 +1,46 @@
 package com.mashup.presentation.feature.chat
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.mashup.domain.usecase.chat.GetChatRoomsUseCase
+import com.mashup.presentation.feature.chat.model.RoomUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * Ssam_D_Android
  * @author jaesung
  * @created 2023/07/09
  */
+@HiltViewModel
+class ChatViewModel @Inject constructor(
+    private val getChatRooms: GetChatRoomsUseCase
+) : ViewModel() {
 
-class ChatViewModel: ViewModel()
+    private val _pagingData: MutableStateFlow<PagingData<RoomUiModel>> =
+        MutableStateFlow(PagingData.empty())
+    val pagingData = _pagingData.asStateFlow()
+
+    fun getChatRooms() {
+        viewModelScope.launch {
+            getChatRooms.execute(Unit).cachedIn(viewModelScope).map { pagingData ->
+                    pagingData.map {room ->RoomUiModel.fromDomainModel(room) }
+                }.collect {
+                    _pagingData.value = it
+                }
+        }
+    }
+
+    sealed class ChatRoomUiState {
+        object Loading : ChatRoomUiState()
+        data class Success(val rooms: PagingData<RoomUiModel>) : ChatRoomUiState()
+        data class Failure(val message: String) : ChatRoomUiState()
+    }
+}
