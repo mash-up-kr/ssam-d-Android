441ce61be15d8b6432cd442567340c7306feedd7
Jaesung Lee <viki0114@naver.com>
2023-07-21 00:56:38 +0900

    [feat] #161 getAccessToken 로직 추가

---
+++ domain/src/main/java/com/mashup/domain/usecase/GetAccessTokenUseCase.kt
@@ -0,0 +1,18 @@
+package com.mashup.domain.usecase
+
+import com.mashup.domain.repository.UserRepository
+import com.mashup.domain.usecase.common.CoroutineUseCase
+import javax.inject.Inject
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/20
+ */
+class GetAccessTokenUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) : CoroutineUseCase<Unit, String>() {
+    override suspend fun invoke(param: Unit): String {
+        return userRepository.getUserAccessToken()
+    }
+}
