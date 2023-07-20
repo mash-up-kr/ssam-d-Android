5cfdb3a3e8c334004ef27f4f398183444e4cfc29
Jaesung Lee <viki0114@naver.com>
2023-07-21 01:14:47 +0900

    [feat] #186 구독 키워드 저장 DataSource 추가

--- data/src/main/java/com/mashup/data/source/remote/source/datasource/RemoteKeywordDataSource.kt
+++ data/src/main/java/com/mashup/data/source/remote/source/datasource/RemoteKeywordDataSource.kt
@@ -1,5 +1,6 @@
 package com.mashup.data.source.remote.source.datasource
 
+import com.mashup.data.source.remote.dto.requestbody.KeywordsRequestBody
 import com.mashup.data.source.remote.dto.responsebody.keyword.KeywordRecommendResponse
 import com.mashup.data.source.remote.service.KeywordService
 import javax.inject.Inject
@@ -15,6 +16,15 @@ class RemoteKeywordDataSource @Inject constructor(
     suspend fun getSubscribeKeywords(): List<String> {
         val response = keywordService.getSubscribeKeywords()
         return response.data ?: throw Exception(response.message)
+    }
+
+    suspend fun postSubscribeKeywords(subscribeKeywords: List<String>): String {
+        val response = keywordService.postSubscribeKeywords(
+            keywordsRequest = KeywordsRequestBody(
+                keywords = subscribeKeywords
+            )
+        )
+        return response.message
     }
 
     suspend fun getRecommendKeyword(content: String): KeywordRecommendResponse {
