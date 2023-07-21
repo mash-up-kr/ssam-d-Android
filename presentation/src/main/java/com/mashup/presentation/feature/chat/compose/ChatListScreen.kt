ba69a40221f6a0aae5f4751ec68784e5c565bb36
Huijiny <jiin20803@gmail.com>
2023-07-22 02:17:54 +0900

    [feat] #190 isChatRead 값 반대로 수정

--- presentation/src/main/java/com/mashup/presentation/feature/chat/compose/ChatListScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/chat/compose/ChatListScreen.kt
@@ -89,7 +89,6 @@ fun ChatRoomItem(
             .clickable { onMessageClick() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        /* 이미지 대신 임시로 추가 */
         GlideImage(
             modifier = Modifier.size(36.dp),
             model = chatRoom.profileImage,
@@ -109,7 +108,7 @@ fun ChatRoomItem(
 
             ChatRoomMessageInfo(
                 recentMessage = chatRoom.recentSignalContent,
-                isNewMessage = chatRoom.isChatRead,
+                isNewMessage = !chatRoom.isChatRead,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 2.dp)
