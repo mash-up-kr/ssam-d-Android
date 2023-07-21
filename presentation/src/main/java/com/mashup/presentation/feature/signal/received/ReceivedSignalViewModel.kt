07c649e2f4b5099b8779d0aec8e89b2252909475
Jaesung Lee <viki0114@naver.com>
2023-07-22 01:10:20 +0900

    [feat] #196 ReceivedSignal Composable 생성

---
+++ presentation/src/main/java/com/mashup/presentation/feature/signal/received/ReceivedSignalViewModel.kt
@@ -0,0 +1,13 @@
+package com.mashup.presentation.feature.signal.received
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/22
+ */
+@HiltViewModel
+class ReceivedSignalViewModel : ViewModel() {
+}
---
+++ presentation/src/main/java/com/mashup/presentation/feature/signal/received/navigation/ReceivedSignalDetailNavigation.kt
@@ -0,0 +1,22 @@
+package com.mashup.presentation.feature.signal.received.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import com.mashup.presentation.navigation.KeyLinkNavigationRoute
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/22
+ */
+
+fun NavController.navigateToReceivedSignalDetail(
+    signalId: Long,
+    navOptions: NavOptions? = null
+) {
+    navigate(
+        route = KeyLinkNavigationRoute.ChatGraph.ReceivedSignalDetailRoute.route
+            .replace("{signalId}", "$signalId"),
+        navOptions = navOptions
+    )
+}
