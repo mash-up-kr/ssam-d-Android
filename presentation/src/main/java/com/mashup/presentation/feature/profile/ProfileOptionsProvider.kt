25a50aa902a5a050a76f4e28a911d351eb4bbf6d
Jaesung Lee <viki0114@naver.com>
2023-07-23 22:19:59 +0900

    [feat] #238 프로필 1차 미배포 기능 제외

--- presentation/src/main/java/com/mashup/presentation/feature/profile/ProfileOptionsProvider.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/profile/ProfileOptionsProvider.kt
@@ -32,24 +32,6 @@ class ProfileOptionsProvider @Inject constructor(
                 )
             )
             add(
-                ProfileViewType.NavigationContent(
-                    description = context.getString(R.string.profile_sent_signal),
-                    route = KeyLinkNavigationRoute.ProfileGraph.SendSignalRoute.route,
-                    color = Mint
-                )
-            )
-            add(
-                ProfileViewType.Header(
-                    description = context.getString(R.string.settings)
-                )
-            )
-            add(
-                ProfileViewType.NotificationContent(
-                    description = context.getString(R.string.notification),
-                    isAgree = profileUiModel.agreeAlarm
-                )
-            )
-            add(
                 ProfileViewType.Header(
                     description = context.getString(R.string.information)
                 )
@@ -64,12 +46,6 @@ class ProfileOptionsProvider @Inject constructor(
                 ProfileViewType.NavigationContent(
                     description = context.getString(R.string.privacy_policy),
                     route = KeyLinkNavigationRoute.ProfileGraph.PrivacyPolicyRoute.route
-                )
-            )
-            add(
-                ProfileViewType.NavigationContent(
-                    description = context.getString(R.string.open_source),
-                    route = KeyLinkNavigationRoute.ProfileGraph.OpenSourceRoute.route
                 )
             )
             add(
