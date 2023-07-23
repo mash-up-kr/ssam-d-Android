8d41f2fc391e1dc5438dddec4e33a2ba0fa3fd6f
Jaesung Lee <viki0114@naver.com>
2023-07-23 18:28:10 +0900

    [feat] #231 개인정보 처리방침, 이용약관 Screen 생성

---
+++ presentation/src/main/java/com/mashup/presentation/feature/profile/policy/PrivacyPolicyScreen.kt
@@ -0,0 +1,24 @@
+package com.mashup.presentation.feature.profile.policy
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/23
+ */
+@Composable
+fun TermsOfServiceRoute(
+    modifier: Modifier = Modifier
+) {
+
+    TermsOfServiceScreen()
+}
+
+@Composable
+private fun TermsOfServiceScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
