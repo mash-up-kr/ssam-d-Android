4d1250e27b0330574070263f4e27d14357f6c5de
Jaesung Lee <viki0114@naver.com>
2023-07-23 16:01:42 +0900

    [feat] #229 NetworkStatusMonitor 클래스 생성

---
+++ presentation/src/main/java/com/mashup/presentation/common/network/NetworkStatusMonitor.kt
@@ -0,0 +1,61 @@
+package com.mashup.presentation.common.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.*
+
+/**
+ * Ssam_D_Android
+ * @author jaesung
+ * @created 2023/07/23
+ */
+class NetworkStatusMonitor(
+    context: Context,
+    private val coroutineScope: CoroutineScope
+) {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    val networkStatus = callbackFlow<NetworkStatus> {
+        val networkStatusCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                trySend(NetworkStatus.NetworkConnected)
+            }
+
+            override fun onLost(network: Network) {
+                trySend(NetworkStatus.NetworkDisconnected)
+            }
+
+            override fun onUnavailable() {
+                trySend(NetworkStatus.NetworkDisconnected)
+            }
+        }
+
+        register(networkStatusCallback)
+
+        awaitClose {
+            unregister(networkStatusCallback)
+        }
+    }.distinctUntilChanged()
+
+    private fun register(networkStatusCallback: ConnectivityManager.NetworkCallback) {
+        connectivityManager.registerDefaultNetworkCallback(networkStatusCallback)
+    }
+
+    private fun unregister(networkStatusCallback: ConnectivityManager.NetworkCallback) {
+        connectivityManager.unregisterNetworkCallback(networkStatusCallback)
+    }
+}
+
+inline fun <R> Flow<NetworkStatus>.map(
+    crossinline onDisconnected: suspend () -> R,
+    crossinline onConnected: suspend () -> R,
+): Flow<R> = map { status ->
+    when (status) {
+        NetworkStatus.NetworkDisconnected -> onDisconnected()
+        NetworkStatus.NetworkConnected -> onConnected()
+    }
+}
