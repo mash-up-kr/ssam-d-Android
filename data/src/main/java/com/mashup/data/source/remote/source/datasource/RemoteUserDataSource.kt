254be1571dbd58ae3ce577529e59fc51ac88608e
Huijiny <jiin20803@gmail.com>
2023-07-30 02:58:26 +0900

    [feat] #261 보낸 시그널 리모트데이터소스 생성

--- data/src/main/java/com/mashup/data/source/remote/source/datasource/RemoteUserDataSource.kt
+++ data/src/main/java/com/mashup/data/source/remote/source/datasource/RemoteUserDataSource.kt
@@ -4,8 +4,12 @@ import com.mashup.data.source.remote.dto.BaseResponse
 import com.mashup.data.source.remote.dto.requestbody.AlarmRequestBody
 import com.mashup.data.source.remote.dto.requestbody.LoginRequestBody
 import com.mashup.data.source.remote.dto.responsebody.LoginResponseBody
-import com.mashup.data.source.remote.dto.responsebody.UserResponseBody
+import com.mashup.data.source.remote.dto.responsebody.user.SentSignalDetailResponse
+import com.mashup.data.source.remote.dto.responsebody.user.SentSignalPagingResponse
+import com.mashup.data.source.remote.dto.responsebody.user.SentSignalResponse
+import com.mashup.data.source.remote.dto.responsebody.user.UserResponseBody
 import com.mashup.data.source.remote.service.UserService
+import com.mashup.domain.model.signal.SentSignal
 import javax.inject.Inject
 
 class RemoteUserDataSource @Inject constructor(
@@ -24,6 +28,7 @@ class RemoteUserDataSource @Inject constructor(
     suspend fun patchNickname(nickname: String) {
         userService.patchNickname(nickname)
     }
+
     suspend fun patchAlarm(isAgree: Boolean) {
         userService.patchAlarm(AlarmRequestBody(isAgree))
     }
@@ -34,5 +39,16 @@ class RemoteUserDataSource @Inject constructor(
 
     suspend fun deleteUser(id: Long) {
         userService.deleteUser(id)
+    }
+
+    suspend fun getSentSignals(pageNumber: Int, pageLength: Int?): SentSignalPagingResponse {
+        val response =
+            userService.getSentSignal(pageNumber = pageNumber, pageLength = pageLength)
+        return response.data ?: throw Exception(response.message)
+    }
+
+    suspend fun getSentSignalDetail(signalId: Long): SentSignalDetailResponse {
+        val response = userService.getSentSignalDetail(signalId)
+        return response.data ?: throw Exception(response.message)
     }
 }
