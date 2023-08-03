b98db6c269e1806b2a375ed8ba747cb19a3d7d35
Jaesung Lee <viki0114@naver.com>
2023-08-03 17:59:14 +0900

    [feat] #254 remoteMessage data에서 파싱하도록 수정

--- app/src/main/java/com/mashup/ssamd/notification/ShowPushNotificationUseCase.kt
+++ app/src/main/java/com/mashup/ssamd/notification/ShowPushNotificationUseCase.kt
@@ -13,10 +13,8 @@ class ShowPushNotificationUseCase @Inject constructor(
     private val notificationBuilder: NotificationBuilder
 ) {
     operator fun invoke(remoteMessage: RemoteMessage) {
-        val title = remoteMessage.notification?.title.orEmpty()
-        val body = remoteMessage.notification?.body.orEmpty()
-//        val title = remoteMessage.data["title"]
-//        val body = remoteMessage.data["body"]
+        val title = remoteMessage.data["title"].orEmpty()
+        val body = remoteMessage.data["body"].orEmpty()
         val notificationType = remoteMessage.data["notiType"].orEmpty()
         val roomId = remoteMessage.data["roomId"].orEmpty()
 
