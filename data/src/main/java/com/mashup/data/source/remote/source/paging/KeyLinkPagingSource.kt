e31a0d122aba83531adb820582d20483c27fea8a
Huijiny <jiin20803@gmail.com>
2023-08-06 22:55:26 +0900

    [feat] #276 페이징 공통 에러파싱

--- data/src/main/java/com/mashup/data/source/remote/source/paging/KeyLinkPagingSource.kt
+++ data/src/main/java/com/mashup/data/source/remote/source/paging/KeyLinkPagingSource.kt
@@ -3,7 +3,14 @@ package com.mashup.data.source.remote.source.paging
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.mashup.domain.base.paging.PagedData
+import com.mashup.domain.exception.ConflictException
+import com.mashup.domain.exception.EmptyListException
+import com.mashup.domain.exception.KeyLinkException
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.delay
+import retrofit2.HttpException
 
 /**
  * Ssam_D_Android
@@ -35,13 +42,21 @@ class KeyLinkPagingSource<T : Any> (
                 prevKey = if (page == INITIAL_PAGE) null else page - 1,
                 nextKey = if (isLastPage) null else page + 1
             )
-        } catch (e: Throwable) {
+        } catch (e: HttpException) {
+            val rawData = e.response()?.errorBody()?.string() ?: ""
+            val builder = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val jsonAdapter: JsonAdapter<KeyLinkException> =
+                builder.adapter(KeyLinkException::class.java)
+            val exception = jsonAdapter.fromJson(rawData)
+            val message = exception?.message
+            LoadResult.Error(KeyLinkException(message))
+        }
+        catch (e: Throwable) {
             LoadResult.Error(e)
         }
     }
-
-    object EmptyListException: Exception()
-
 
     companion object {
         private const val INITIAL_PAGE = 1
