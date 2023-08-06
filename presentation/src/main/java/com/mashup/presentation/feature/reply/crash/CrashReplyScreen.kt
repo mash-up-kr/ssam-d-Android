0d3a9151c389d6c0551e7e3f792d00787c1fffa5
Huijiny <jiin20803@gmail.com>
2023-08-06 15:42:55 +0900

    [feat] #273 크래시 답장 presentation 연결

--- presentation/src/main/java/com/mashup/presentation/feature/reply/crash/CrashReplyScreen.kt
+++ presentation/src/main/java/com/mashup/presentation/feature/reply/crash/CrashReplyScreen.kt
@@ -6,12 +6,15 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mashup.presentation.R
 import com.mashup.presentation.feature.reply.ReplyContent
 import com.mashup.presentation.ui.common.KeyLinkContentLengthDialog
@@ -33,15 +36,30 @@ fun CrashReplyRoute(
     navigateToHome: () -> Unit,
     onShowSnackbar: (String, SnackbarDuration) -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: CrashReplyViewModel = hiltViewModel()
 ) {
+    val event by viewModel.eventFlow.collectAsStateWithLifecycle(initialValue = CrashReplyUiEvent.Idle)
     var reply by rememberSaveable { mutableStateOf("") }
+
+    LaunchedEffect(event) {
+        when(event) {
+            CrashReplyUiEvent.SendReplySuccess -> navigateToHome()
+            is CrashReplyUiEvent.SendReplyFailed -> {
+                onShowSnackbar(
+                    (event as CrashReplyUiEvent.SendReplyFailed).message ?: "",
+                    SnackbarDuration.Short
+                )
+            }
+            else -> {}
+        }
+    }
+
     CrashReplyScreen(
         reply = reply,
         onReplyTextChange = { reply = it },
         onClickBack = onClickBack,
         onSendClick = {
-            /* viewModel 호출 후 클릭 이벤트 구독 필요 */
-            navigateToHome() // 제거
+            viewModel.sendCrashReply(crashId = crashId, content = reply)
         },
         onShowSnackbar = onShowSnackbar,
         modifier = modifier
