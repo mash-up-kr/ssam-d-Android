d1dbfff9653a2dcefda803bab2eefb63aa40c669
Huijiny <jiin20803@gmail.com>
2023-08-06 15:42:55 +0900

    [feat] #273 크래시 답장 presentation 연결

---
+++ presentation/src/main/java/com/mashup/presentation/feature/reply/crash/CrashReplyViewModel.kt
@@ -0,0 +1,44 @@
+package com.mashup.presentation.feature.reply.crash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.domain.usecase.crash.ReceivedCrashReplyParams
+import com.mashup.domain.usecase.crash.SendCrashReplyUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CrashReplyViewModel @Inject constructor(
+    private val sendCrashReplyUseCase: SendCrashReplyUseCase
+) : ViewModel() {
+
+    private val _eventFlow: MutableSharedFlow<CrashReplyUiEvent> =
+        MutableSharedFlow()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun sendCrashReply(crashId: Long, content: String) {
+        viewModelScope.launch {
+            val replyRequestParams = ReceivedCrashReplyParams(
+                crashId = crashId,
+                content = content
+            )
+
+            sendCrashReplyUseCase.execute(param = replyRequestParams)
+                .onSuccess {
+                    _eventFlow.emit(CrashReplyUiEvent.SendReplySuccess)
+                }
+                .onFailure {
+                    _eventFlow.emit(CrashReplyUiEvent.SendReplyFailed(it.message))
+                }
+        }
+    }
+}
+
+sealed interface CrashReplyUiEvent {
+    object Idle : CrashReplyUiEvent
+    object SendReplySuccess : CrashReplyUiEvent
+    data class SendReplyFailed(val message: String?) : CrashReplyUiEvent
+}
